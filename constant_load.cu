72ed266dc7abe2cc2f941eed76d9fa864e00da70
Emerson Jacobson <emwjacobson@gmail.com>
2021-05-18 14:59:40 -0700

    Changed to doing matrix multiplications instead of custom kernel

--- constant_load.cu
+++ constant_load.cu
@@ -1,88 +1,72 @@
 #include <cuda_runtime.h>
 #include <cuda_fp16.h>
+#include <cublas_v2.h>
 #include <cstdio>
 #include <chrono>
 #include <random>
-
-// Flops = (iterations * num_ops_per_item * blocks * threads) / time_seconds
-// (14586 * 3 * 128 * 1024 ) / 5 = 1.14x10^9 FLOPS
-
-__global__ void testKernel(float* A, float* B, float* C) {
-    // Add timer here
-    // https://github.com/zchee/cuda-sample/blob/master/0_Simple/clock/clock.cu
-
-    int idx = threadIdx.x + blockIdx.x * blockDim.x;
-
-    C[idx] = .997 * A[idx] + .998 * B[idx]; // 3 Floating Point Operations
-}
+#include <iostream>
 
 void print_usage(int argc, char* argv[]) {
-    printf("Usage: %s [# blocks] [# threads/block] [seconds]\n", argv[0]);
+    printf("Usage: %s [dim] [runtime]\n", argv[0]);
 }
 
 int main(int argc, char* argv[]) {
-    if (argc != 4) {
+    if (argc != 3) {
         print_usage(argc, argv);
         return 0;
     }
 
-    int blocks = std::stoi(argv[1]);
-    int threads = std::stoi(argv[2]);
-    int runtime = std::stoi(argv[3]); // In seconds
-
-    if (blocks <= 0 || threads <= 0) {
-        print_usage(argc, argv);
-        return 0;
-    }
+    int matrix_dim = std::stoi(argv[1]);
+    int runtime = std::stoi(argv[2]);
 
     // Allocate memory on host and fill with random numbers
-    float* hA = new float[threads*blocks];
-    float* hB = new float[threads*blocks];
-    float* hC = new float[threads*blocks];
-    for(int i = 0; i < threads*blocks; i++) {
-        hA[i] = float(std::rand())/float((RAND_MAX));
-        hB[i] = float(std::rand())/float((RAND_MAX));
+    float *A, *B, *C;
+    cudaMallocManaged(&A, sizeof(float) * matrix_dim * matrix_dim);
+    cudaMallocManaged(&B, sizeof(float) * matrix_dim * matrix_dim);
+    cudaMallocManaged(&C, sizeof(float) * matrix_dim * matrix_dim);
+    for(int i = 0; i<matrix_dim; i++) {
+        A[i] = float(std::rand())/float((RAND_MAX));
+        B[i] = float(std::rand())/float((RAND_MAX));
+        C[i] = float(std::rand())/float((RAND_MAX));
     }
 
-    // Allocate memory on GPU and copy data
-    float *dA, *dB, *dC;
-    cudaMalloc(&dA, sizeof(float) * threads * blocks);
-    cudaMalloc(&dB, sizeof(float) * threads * blocks);
-    cudaMalloc(&dC, sizeof(float) * threads * blocks);
-    cudaMemcpy(dA, hA, sizeof(float) * threads * blocks, cudaMemcpyHostToDevice);
-    cudaMemcpy(dB, hB, sizeof(float) * threads * blocks, cudaMemcpyHostToDevice);
-
     // Initialize timing variables and start timer
-    float time_ms, final_time;
+    float time_ms;
     cudaEvent_t gpu_start, gpu_stop;
     cudaEventCreate(&gpu_start);
     cudaEventCreate(&gpu_stop);
-    cudaEventRecord(gpu_start);
+
+    cublasHandle_t handle;
+    cublasCreate(&handle);
 
     // Run computations
     int i = 0;
-    printf("Using %i blocks with %i threads/block for %i seconds.\n", blocks, threads, runtime);
+    float alpha = 1, beta = 0;
+    printf("Using a %i x %i matrix for %i seconds.\n", matrix_dim, matrix_dim, runtime);
     std::chrono::system_clock::time_point end = std::chrono::system_clock::now() + std::chrono::seconds(runtime);
+
+    cudaEventRecord(gpu_start);
     while (std::chrono::system_clock::now() < end) {
-        testKernel<<<blocks, threads>>>(dA, dB, dC);
+        cublasSgemm(handle,
+                CUBLAS_OP_N,
+                CUBLAS_OP_N,
+                matrix_dim,
+                matrix_dim,
+                matrix_dim,
+                &alpha, A, matrix_dim,
+                B, matrix_dim,
+                &beta, C, matrix_dim);
         cudaDeviceSynchronize();
         i++;
-        // cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, i, i, i, &alpha, d_A, i, d_B, i, &beta, d_C, i);
     }
+    cudaEventRecord(gpu_stop);
 
     // Calculate runtime
-    cudaEventRecord(gpu_stop);
     cudaEventSynchronize(gpu_stop);
     cudaEventElapsedTime(&time_ms, gpu_start, gpu_stop);
-    final_time = time_ms/1000;
     printf("Actual time: %fms over %i iterations\n", time_ms, i);
 
-    cudaMemcpy(hC, dC, sizeof(float) * threads * blocks, cudaMemcpyDeviceToHost);
-
-    // for(int i=0; i < threads*blocks; i++) printf("%f\n", hC[i]);
-
-    cudaFree(dA);
-    cudaFree(dB);
-    cudaFree(dC);
-    delete hA, hB, hC;
+    cudaFree(A);
+    cudaFree(B);
+    cudaFree(C);
 }
